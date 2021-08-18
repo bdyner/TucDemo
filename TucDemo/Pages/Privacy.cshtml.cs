b7a7cc9018ce492e95cc699ef3dc7e80fcbfb51a
Lenovo <Lenovo@DESKTOP-E9T3PVO>
2021-08-18 14:57:45 +0200

    Add project files.

---
+++ TucDemo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TucDemo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        public string CurrentWeekDay { get; set; }
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+       
+        public void OnGet()
+        {
+            var svenska = new CultureInfo("sv-SE");
+            CurrentWeekDay = DateTime.Now.ToString("dddd", svenska);
+        
+        }
+    }
+}
